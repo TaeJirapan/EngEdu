4fc0ddbc29cadfceed7561e458547ef1e2ac2c3c
TaeJirapan <Jirapanza_14@hotmail.com>
2017-08-31 15:06:20 +0700

    2nd  year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -14,6 +14,7 @@ namespace EngEdu
             Console.WriteLine("Year 255x semester 2, I learn to programp c sharp.");
             Console.WriteLine("Year 2560 semester 1, I have to regrade on some course.");
             Console.WriteLine("Year 2560 semester 2, I have to relax on some course.");
+            Console.WriteLine("Year 2561 semester 1, I let go to clear some course");
         }
     }
 }
